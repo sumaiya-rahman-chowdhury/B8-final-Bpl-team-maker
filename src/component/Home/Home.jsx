5609bd7635731a50d45a5b78111ed03d1d08607b
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-09-13 10:17:38 -0700

    first commit

---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,17 @@
+
+import Cart from '../Cart/Cart';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div >
+            <div className=' flex justify-center items-center h-screen'>
+                <div className=' w-[250px] h-[300px] border-[1px] border-gray-400'>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
