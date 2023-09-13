5431525c28c8b79936f024a0abe78b1cbb009af8
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-09-13 14:47:02 -0700

    selected actors to cart

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,9 +1,18 @@
+/* eslint-disable react/prop-types */
 
 
-const Cart = () => {
+const Cart = ({selectedActors}) => {
+    console.log(selectedActors)
+
     return (
         <div>
-           
+            <h3 className=" text-xl font-bold">Total Selected : {selectedActors.length}</h3>
+           {
+            selectedActors.map((selectedActor,idx)=><div className=" mt-4 flex gap-4 items-center justify-between" key={idx}><h1 className=" font-bold">{selectedActor.name}</h1>
+            <img src={selectedActor.image} alt="" className=" w-12" />
+               </div> 
+                )
+           }
         </div>
     );
 };
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -6,6 +6,7 @@ import './Home.css'
 const Home = () => {
 
     const [actors,setActors] = useState([]);
+    const [selectedActors,setSelectedActors] = useState([])
 
     useEffect(()=>{
         fetch('fakeData.json')
@@ -13,28 +14,48 @@ const Home = () => {
         .then(data=>setActors(data))
     },[]);
 
-    console.log(actors)
+    
 
+    const handleSelectActor = (actor)=>{
+
+        const isSelected = selectedActors.find(item=> item.id === actor.id) ;
+
+        let count = actor.salary ;
+        if(isSelected){
+            return alert('Selected')
+        }
+        else{ 
+
+            selectedActors.forEach((item)=>{
+                count = count + item.salary ;
+            })
+            console.log(count)
+        const actorList = [...selectedActors,actor]
+        setSelectedActors(actorList);
+        }
+
+    }
+    // console.log(selectedActors)
 
     return (
         <div >
-            <div className='home-container flex justify-between flex-wrap h-screen mt-10'>
+            <div className='home-container flex gap-x-10 flex-wrap h-screen mt-10'>
                <div className='card-container w-[800px] text-white flex flex-wrap gap-5 justify-center'>
 
                 {
                     actors.map((actor,idx)=>
-                        <div key={idx} className='cards transition-transform transform hover:scale-110 w-[250px] shadow-lg shadow-gray-400 border-[1px] border-gray-400 text-center space-y-2'>
+                        <div key={idx} className='cards transition-transform transform hover:scale-110 w-[250px] border-[1px] border-gray-400 text-center space-y-2'>
                         <div className='flex justify-center items-center '>
                         <img src={actor.image} alt="" className=' w-20 h-20 rounded-full mt-3'/>
                         </div>
                         <h2 className=' font-bold text-xl'>{actor.name}</h2>
                         <p className=' text-center px-3'><small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo maxime, laborum nemo minima iste eos impedit debitis. Nulla, itaque soluta!</small></p>
                         <div className=' flex gap-x-10 justify-center '>
-                            <p>salary: {actor?.salary ? actor.salary : "Null"} </p>
+                            <p>salary: {actor?.salary ? actor.salary : "Null"} $ </p>
                             <p>{actor.role}</p>
                         </div>
                         <div className='pb-3'>
-                        <button className=' bg-gray-700 text-white px-6 py-2 rounded '>Select</button>
+                        <button onClick={()=>handleSelectActor(actor)} className=' bg-gray-700 text-white px-6 py-2 rounded '>Select</button>
                         </div>
                         </div>)
                 }
@@ -42,7 +63,7 @@ const Home = () => {
                 
                </div>
                <div className="cart text-white">
-                    this is cart
+                   <Cart selectedActors={selectedActors}></Cart>
                 </div>
             </div>
         </div>
